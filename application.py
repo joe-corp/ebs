e4099177ec195a19ec8d9a7343d7970da9840bac
Joseph Ojo <70476696+OjoFemiJoseph@users.noreply.github.com>
2024-08-18 07:20:50 -0500

    Update application.py

--- application.py
+++ application.py
@@ -11,37 +11,17 @@ load_dotenv()
 
 application = FastAPI()
 
-sts_client = boto3.client('sts')
 
-response = sts_client.assume_role(
-    RoleArn='arn:aws:iam::635933673805:role/devops_role',
-    RoleSessionName='MySessionName'
-)
-
-credentials = response['Credentials']
-
-assumed_role_session = boto3.Session(
-    aws_access_key_id=credentials['AccessKeyId'],
-    aws_secret_access_key=credentials['SecretAccessKey'],
-    aws_session_token=credentials['SessionToken']
-)
-
-s3 = assumed_role_session.client('s3')
-
-@app.get('/')
+@application.get('/')
 def index():
     return 200, "Hello World"
 
-@app.post("/upload_image")
+@application.post("/upload_image")
 async def upload_image(file: UploadFile = File(...)):
     bucket_name = os.environ['BUCKET_NAME']
 
     try:
-        image_data = await file.read()
-        image_key = f"images/{file.filename}"
-
-        s3.put_object(Body=image_data, Bucket=bucket_name, Key=image_key, ContentType=file.content_type)
-
+        
         return JSONResponse(status_code=201, content={"message": "Image uploaded successfully"})
     except Exception as e:
         print(f"Error uploading image: {e}")
