43f5f42b0d4e27617c8b86762f34e3d7daa8532b
Joseph Ojo <70476696+OjoFemiJoseph@users.noreply.github.com>
2024-08-18 07:42:51 -0500

    Update application.py

--- application.py
+++ application.py
@@ -19,16 +19,6 @@ def index():
 def index():
     return 200, "Hello World"
 
-@application.post("/upload_image")
-async def upload_image(file: UploadFile = File(...)):
-    bucket_name = os.environ['BUCKET_NAME']
-
-    try:
-        
-        return JSONResponse(status_code=201, content={"message": "Image uploaded successfully"})
-    except Exception as e:
-        print(f"Error uploading image: {e}")
-        raise HTTPException(status_code=400, detail="Error uploading image")
 
 if __name__ == "__main__":
     application.debug = True
