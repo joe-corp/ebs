e0498c68e8c794cc2b7e3a3a29114fad457a4990
Joseph Ojo <70476696+OjoFemiJoseph@users.noreply.github.com>
2024-08-25 09:01:20 +0000

    update

---
+++ test_aaplication.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from application import application
+
+client = TestClient(application)
+
+def test_index():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == "Hello World"
